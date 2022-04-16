774c2f20df11b6c7fb65e37c2aa5a444035d2cbc
Mohammed <medo.hassan0100@gmail.com>
2022-04-16 22:02:28 +0200

    migrations

---
+++ database/migrations/2022_04_16_193410_create_appointments_times_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments_times', function (Blueprint $table) {
+            $table->id();
+            $table->time('time_from');
+            $table->time('time_to');
+            $table->float('intervals');
+            $table->integer('doctor_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments_times');
+    }
+};
---
+++ database/migrations/2022_04_16_193646_create_diseases_activities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diseases_activities', function (Blueprint $table) {
+            $table->id();
+            $table->integer('disease_id');
+            $table->integer('activity_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diseases_activities');
+    }
+};
